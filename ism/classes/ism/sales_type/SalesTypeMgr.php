59d95e9832cd862bfe379caba39b00fe6ab68d69
나영욱 <dyang34@naver.com>
2022-03-29 14:33:03 +0900

    판매 유형 관리 추가.

---
+++ ism/classes/ism/sales_type/SalesTypeMgr.php
@@ -0,0 +1,206 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']."/ism/classes/ism/sales_type/SalesTypeDao.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/ism/classes/cms/db/A_Mgr.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/ism/classes/cms/db/DbUtil.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/ism/classes/cms/db/WhereQuery.php";
+
+class SalesTypeMgr extends A_Mgr
+{
+    private static $instance = null;
+    
+    private function __construct() {
+        // getInstance() 이용.
+    }
+    
+    static function getInstance() {
+        if ( self::$instance == null ) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+    
+    function getByKey($key) {
+        
+        $row = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            $row = SalesTypeDao::getInstance()->selectByKey($db, $key);
+            
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $row;
+    }
+    
+    function getFirst($wq) {
+        
+        $row = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            $row = SalesTypeDao::getInstance()->selectFirst($db, $wq);
+
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+        
+      @ $db->close();
+        return $row;
+    }
+    
+    /*
+     *	$result 사용후 반드시 @ $result->free(); 해줘야 한다.
+     */
+    function getList($wq) {
+        
+        $result = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            $result = SalesTypeDao::getInstance()->select($db, $wq);
+            
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $result;
+    }
+    
+    /*
+     *	$result 사용후 반드시 @ $result->free(); 해줘야 한다.
+     */
+    function getListPerPage($wq, $pg) {
+        
+        $result = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            $pg->setTotalCount(SalesTypeDao::getInstance()->selectCount($db, $wq));
+            $result = SalesTypeDao::getInstance()->selectPerPage($db, $wq, $pg);
+            
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $result;
+    }
+
+    function getCount($wq) {
+        
+        $result = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            $result = SalesTypeDao::getInstance()->selectCount($db, $wq);
+            
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $result;
+    }
+    
+    function exists($wq) {
+        
+        $result = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            $result = SalesTypeDao::getInstance()->exists($db, $wq);
+            
+        } catch(Exception $e) {
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $result;
+    }
+    
+    function add($arrVal) {
+        
+        $isOk = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            //            $this->startTran($db);
+            
+            $isOk = SalesTypeDao::getInstance()->insert($db, $arrVal);
+            
+            //            $this->commit($db);
+            
+        } catch(Exception $e) {
+            //            $this->rollback($db);
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $isOk;
+    }
+    
+    function edit($uq, $key) {
+        
+        $isOk = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            //$this->startTran($db);
+            
+            $isOk = SalesTypeDao::getInstance()->update($db, $uq, $key);
+            
+            //$this->commit($db);
+            
+        } catch(Exception $e) {
+            //$this->rollback($db);
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $isOk;
+    }
+    
+    function delete($key) {
+        
+        $isOk = null;
+        $db = null;
+        
+        try {
+            $db = DbUtil::getConnection();
+            
+            //$this->startTran($db);
+            
+            $isOk = SalesTypeDao::getInstance()->delete($db, $key);
+            
+            //$this->commit($db);
+            
+        } catch(Exception $e) {
+            //$this->rollback($db);
+            echo $e->getMessage();
+        }
+        
+        @ $db->close();
+        return $isOk;
+    }
+}
+?>
---
+++ ism/classes/ism/status/StatusDao.php
@@ -0,0 +1,156 @@
+<?php 
+require_once $_SERVER['DOCUMENT_ROOT']."/ism/classes/cms/db/A_Dao.php";
+
+class StatusDao extends A_Dao
+{
+	private static $instance = null;
+
+	private function __construct() {
+	    // getInstance() 이용.
+	}
+	
+	static function getInstance() {
+		if ( self::$instance == null ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+
+	function selectByKey($db, $key) {
+		 
+		$sql =" select ims_idx, title_status "
+			 ." from ism_mst_status a "
+			 ." where ims_idx = ".$this->quot($db, $key)
+		 	 ;
+		
+		$row = null;
+		$result = $db->query($sql);
+		if ( $result->num_rows > 0 ) {
+		    $row = $result->fetch_assoc();
+		}
+		
+		@ $result->free();
+
+        return $row;
+	}
+
+	function selectFirst($db, $wq) {
+
+		$sql =" select ims_idx, title_status "
+			 ." from ism_mst_status"
+			 .$wq->getWhereQuery()
+			 .$wq->getOrderByQuery()
+			 ;
+		
+		$row = null;
+
+		$result = $db->query($sql);
+		if ( $result->num_rows > 0 ) {
+		    $row = $result->fetch_assoc();
+		}
+		
+		@ $result->free();
+		
+		return $row;
+	}
+
+	function select($db, $wq) {
+	    
+	    $sql =" select ims_idx, title_status "
+	         ." from ism_mst_status"
+	         .$wq->getWhereQuery()
+	         .$wq->getOrderByQuery()
+	         ;
+
+        return $db->query($sql);
+	}
+	
+	function selectPerPage($db, $wq, $pg) {
+		
+		$sql =" select @rnum:=@rnum+1 as rnum, r.* from ("
+			 ."		select @rnum:=0, ims_idx, title_status "
+			 ."		from ism_mst_status"
+	         .$wq->getWhereQuery()
+	         .$wq->getOrderByQuery()
+	         ."		limit ".$pg->getStartIdx().", ".$pg->getPageSize()
+			 ." ) r"
+			 ;
+
+        return $db->query($sql);
+	}
+	
+	function selectCount($db, $wq) {
+
+		$sql =" select count(*) cnt"
+			 ." from ism_mst_status a "
+			 .$wq->getWhereQuery()
+			 ;
+		
+		$row = null;
+		$result = $db->query($sql);
+		if ( $result->num_rows > 0 ) {
+		    $row = $result->fetch_assoc();
+		}
+		
+		@ $result->free();
+		
+		return $row["cnt"];
+	}
+	
+	function exists($db, $wq) {
+
+		$sql =" select count(*) cnt"
+			 ." from ism_mst_status a "
+			 .$wq->getWhereQuery()
+			 ;
+
+		$row = null;
+		$result = $db->query($sql);
+		if ( $result->num_rows > 0 ) {
+		    $row = $result->fetch_assoc();
+		}
+		
+		@ $result->free();
+		
+/*		
+		$result = mysql_query($sql);
+		if ( mysql_num_rows($result) > 0 ) {
+		    $row = mysql_fetch_assoc($result);
+		}
+		
+		@ mysql_free_result($result);
+*/		
+		if ( $row["cnt"] > 0 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	function insert($db, $arrVal) {
+	    
+	    $sql =" insert into ism_mst_status(title_status)"
+	        ." values ('".$this->checkMysql($db, $arrVal["title_status"])
+	        ."')"
+	            ;
+	            
+        return $db->query($sql);
+	}
+	
+	function update($db, $uq, $key) {
+	    
+	    $sql =" update ism_mst_status"
+	        .$uq->getQuery($db)
+	        ." where ims_idx = ".$this->quot($db, $key);
+	        
+	        return $db->query($sql);
+	}
+	
+	function delete($db, $key) {
+	    
+	    $sql = "delete from ism_mst_status where ims_idx = ".$this->quot($db, $key);
+	    
+	    return $db->query($sql);
+	}	
+}
+?>
